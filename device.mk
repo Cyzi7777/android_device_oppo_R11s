74d05396675d48fbd716fd104360b873b2e8d266
Cyzi7777 <Cyzi7777@163.com>
2023-08-04 18:30:01 +0800

    R11s: Build required display libs

--- device.mk
+++ device.mk
@@ -47,6 +47,10 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     Snap
 
+# Display
+PRODUCT_PACKAGES += \
+    libvulkan
+
 # HIDL
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/manifest.xml:system/etc/manifest.xml
